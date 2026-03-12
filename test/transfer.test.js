1ba208621b694ecbf3edbdcb383f63bc1991e48b
Luiz <luizitow@hotmail.com>
2026-03-11 23:14:10 -0400

    Adding transfer tests

---
+++ test/transfer.test.js
@@ -0,0 +1,56 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Transfers', () => {
+    describe('POST /transferencias', () => {
+        it('Should return success 201 when transfer is equals or higher than R$ 10,00', async () => {
+
+            // Get token from login
+            const responseLogin = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({ 'username': 'luiz.neto', 'senha': '123456' }); //Body from Swagger
+
+            const token = responseLogin.body.token;
+
+            const response = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    contaOrigem: 1,
+                    contaDestino: 2,
+                    valor: 11,
+                    token: ""
+                });
+
+            // Add assertions here
+            expect(response.status).to.equal(201);
+
+        });
+
+        it('Should return fail 422 when transfer is lower than R$ 10,00', async () => {
+            const responseLogin = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({ 'username': 'luiz.neto', 'senha': '123456' }); //Body from Swagger
+
+            const token = responseLogin.body.token;
+
+            const response = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    contaOrigem: 1,
+                    contaDestino: 2,
+                    valor: 8,
+                    token: ""
+                });
+
+            // Add assertions here
+            expect(response.status).to.equal(422);
+
+        });
+    });
+});
